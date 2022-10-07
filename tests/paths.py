9a08140b8325986a1dfa3f36d897e39066aa994c
pwoolvett <pablowoolvett@gmail.com>
2022-10-07 15:42:41 -0300

    test: add tracker and analytics fixture data

--- tests/paths.py
+++ tests/paths.py
@@ -11,9 +11,15 @@ from pythia.paths import PEOPLESEGNET
 TESTS = Path(__file__).parent
 FIXTURE_DATA = TESTS / "fixture_data"
 FIXTURE_PIPELINES = FIXTURE_DATA / "pipelines"
+FIXTURE_CONFIGS = FIXTURE_DATA / "configs"
 PROJECT_ROOT = TESTS.parent
 README = PROJECT_ROOT / "README.md"
 DOCKER = PROJECT_ROOT / "docker"
+
+DPA = DS_PATH / "sources/deepstream_python_apps"
+
+ANALYTICS = FIXTURE_CONFIGS / "analytics.conf"
+TRACKER = FIXTURE_CONFIGS / "tracker_NvDCF_perf.yml"
 
 __all__ = [
     "TESTS",
@@ -27,4 +33,7 @@ __all__ = [
     "PROJECT_ROOT",
     "README",
     "DOCKER",
+    "DPA",
+    "TRACKER",
+    "ANALYTICS",
 ]
